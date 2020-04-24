e4f46f5dc8154cc165ac467432572b7e7f9c18e2
alsalganik <63377213+alsalganik@users.noreply.github.com>
2020-04-24 20:26:51 +0300

    Add files via upload
    Added mpi

--- task.f90
+++ task.f90
@@ -1,66 +1,123 @@
-module Task
+module task
+  use mpi
   implicit none
-  contains
-
+contains
   subroutine GetMaxCoordinates(A, x1, y1, x2, y2)
     implicit none
     real(8), intent(in), dimension(:,:) :: A
     integer(4), intent(out) :: x1, y1, x2, y2
-    integer(4) :: n, L, R, Up, Down, m, tmp
-    real(8), allocatable :: current_column(:)
-    real(8) :: current_sum, max_sum
+    integer(4) :: n
+    integer(4) :: L, R, Up, Down
+    integer(4) :: m, tmp
+    real(8), allocatable :: currcolumn(:), B(:,:)
+    real(8) :: currsum, maxsum, global_maxsum
+    logical :: istrans
+    integer(4) :: mpiErr, mpiSize, mpiRank, tmp_mpiRank, global_mpiRank
+
+    call mpi_comm_size(mpi_comm_world, mpiSize, mpiErr)
+
+    call mpi_comm_rank(mpi_comm_world, mpiRank, mpiErr)
 
     m = size(A, dim=1)
     n = size(A, dim=2)
-    allocate(current_column(m))
+    istrans = .FALSE.
+
+    if (m < n) then
+       istrans = .TRUE.
+       B = transpose(A)
+       m = size(B, 1)
+       n = size(B, 2)
+    else
+       B = A
+    endif
+
+    allocate(currcolumn(m))
+
+    maxsum=B(1, 1)
     x1=1
     y1=1
     x2=1
     y2=1
-    max_sum = A(1,1)
-    do L = 1, n
-      current_column = A(:, L)
-      do R = L, n
-        if (R > L) then
-          current_column = current_column + A(:, R)
-        endif
-        call FindMaxInArray(current_column, current_sum, Up, Down)
-        if (current_sum > max_sum) then
-          max_sum = current_sum
-          x1 = Up
-          x2 = Down
-          y1 = L
-          y2 = R
-        endif
-      end do
-    end do
-    deallocate(current_column)
-  end subroutine
 
-  subroutine FindMaxInArray(A, Summ, Up, Down)
-    implicit none
+    do L = mpiRank + 1, n, mpiSize
+       currcolumn = B(:, L)
+       do R=L, n
+
+          if (R > L) then
+             currcolumn = currcolumn + B(:, R)
+          endif
+
+          call FindMaxInArray(currcolumn, currsum, Up, Down)
+
+          if (currsum > maxsum) then
+             maxsum = currsum
+             x1 = Up
+             x2 = Down
+             y1 = L
+             y2 = R
+          endif
+
+       end do
+    end do
+
+    call mpi_reduce(maxsum, global_maxsum, 1, mpi_real8, mpi_max, 0, mpi_comm_world, mpiErr)
+
+    call mpi_bcast(global_maxsum, 1, mpi_real8, 0, mpi_comm_world, mpiErr)
+
+    tmp_mpiRank = -1
+    if (maxsum .eq. global_maxsum) then
+       tmp_mpiRank = mpiRank
+    endif
+
+    call mpi_allreduce(tmp_mpiRank, global_mpiRank, 1, mpi_integer4, mpi_max, mpi_comm_world, mpiErr)
+
+    call mpi_bcast(x1, 1, mpi_real8, global_mpiRank, mpi_comm_world, mpiErr)
+    call mpi_bcast(x2, 1, mpi_real8, global_mpiRank, mpi_comm_world, mpiErr)
+    call mpi_bcast(y1, 1, mpi_real8, global_mpiRank, mpi_comm_world, mpiErr)
+    call mpi_bcast(y2, 1, mpi_real8, global_mpiRank, mpi_comm_world, mpiErr)
+
+    deallocate(currcolumn)
+
+    if (istrans) then
+       tmp = x1
+       x1 = y1
+       y1 = tmp
+
+       tmp = y2
+       y2 = x2
+       x2 = tmp
+    endif
+
+  end subroutine GetMaxCoordinates
+
+
+  subroutine FindMaxInArray(a, Sum, Up, Down)
     real(8), intent(in), dimension(:) :: A
     integer(4), intent(out) :: Up, Down
-    real(8), intent(out) :: Summ
+    real(8), intent(out) :: Sum
     real(8) :: cur_sum
     integer(4) :: minus_pos, i
 
-    Summ = A(1)
+    Sum = a(1)
     Up = 1
     Down = 1
     cur_sum = 0
     minus_pos = 0
+
     do i=1, size(A)
-      cur_sum = cur_sum + A(i)
-      if (cur_sum > Summ) then
-        Summ = cur_sum
-        Up = minus_pos + 1
-        Down = i
-      endif
-      if (cur_sum < 0) then
-        cur_sum = 0
-        minus_pos = i
-      endif
+       cur_sum = cur_sum + A(i)
+       if (cur_sum > Sum) then
+          Sum = cur_sum
+          Up = minus_pos + 1
+          Down = i
+       endif
+
+       if (cur_sum < 0) then
+          cur_sum = 0
+          minus_pos = i
+       endif
+
     enddo
-  end subroutine
-end module
+  end subroutine FindMaxInArray
+
+end module task
